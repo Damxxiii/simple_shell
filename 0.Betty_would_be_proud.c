19f3a01a1a088a11588bcc000b2d354daf484e16
Damxiii <callmedom23@gmail.com>
2023-10-07 05:23:51 -0700

    0.Betty_would_be_proud.c

---
+++ 0.Betty_would_be_proud.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints length of string
+ * Return: length of string
+ */
+int main(void)
+{
+	int i;
+	char s[] = "Programming is fun";
+
+	for (i = 0; s[i] != '\0'; ++i)
+
+	printf("Length of the string: %d", i);
+	return (0);
+}
+
