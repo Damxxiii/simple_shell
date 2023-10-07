cb40ee4a8f1aa83472bea5babed335ef5db741a3
enuel21 <emmaonana18@gmail.com>
2023-10-07 16:37:12 -0700

    displays a prompt each time a command is executed and ill change the execlp()

---
+++ prompt.c
@@ -0,0 +1,8 @@
+#include "shell.h"
+/*
+function that displays the prompt shown in the terminal
+*/
+void display_prompt(void)
+{
+    my_printf("nueldom$");
+}
--- shell.h
+++ shell.h
@@ -1,7 +1,17 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <string.h>
 
 
+void my_printf(const char* format);
+void display_prompt(void);
+void execute_command(const char* command);
+void parse_command(char* command, size_t size);
 
 #endif
