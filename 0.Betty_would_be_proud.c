ea24281b274e18d9ca91abaa0d101e9737ed75fd
Damxiii <callmedom23@gmail.com>
2023-10-18 08:31:14 -0700

    0.Betty_would_be_proud.c

--- 0.Betty_would_be_proud.c
+++ 0.Betty_would_be_proud.c
@@ -1,27 +1,16 @@
 #include <stdio.h>
 /**
- * print_numbers -  prints numbers
- * @numbers: integers to print
- * Return: 0
+ * main - prints its name, followed by a new line
+ *
+ *
+ * Return: 0 on success
  */
-void print_numbers(int numbers[])
-{
-	int i;
 
-	for (int i = 0; i < max_size; i++)
-	{
-		printf("%d ", numbers[i]);
-	}
-	printf("\n");
-}
-/**
- * main - print numbers
- * Return: 0
- */
 int main(void)
 {
-	int numbers[max_size] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+	char str[] = "Dom&Nuel";
 
-	print_numbers(numbers);
+	printf("%s\n", str);
 	return (0);
+
 }
