597351f2262c0ae214b383c5a61392ada13a5f01
Damxiii <callmedom23@gmail.com>
2023-10-18 10:28:27 -0700

    prompt.c

--- prompt.c
+++ prompt.c
@@ -1,19 +1,21 @@
 #include "shell.h"
-/*
-function that displays the prompt shown in the terminal
-*/
-void display_prompt(void)
+/**
+ * main - prints a command prompt
+ *
+ * Return: void
+ */
+int main(void)
 {
-    char input;
+	char *buf;
+	size_t n;
 
-    while (1)
-        {
-            my_printf("nueldom$");
-            if (fgets(input, sizeof(input), stdin) == NULL)
-            {
-                printf("EOF..Exiting..\n");
-                break;
-            }
-        }
-    return (0);
+	buf = malloc(sizeof(char));
+	printf("Welcome, Enter your name");
+	getline(&buf, &n, stdin);
+	printf("Name : %s", buf);
+	printf("Buffer size: %ld\n", n);
+
+	free(buf);
+
+	return (0);
 }
