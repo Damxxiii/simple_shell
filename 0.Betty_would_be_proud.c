2465d1dfb2ba224a0d340ea4b265faf5c6cea0ed
Damxiii <callmedom23@gmail.com>
2023-10-18 07:52:10 -0700

    0.Betty_would_be_proud.c

--- 0.Betty_would_be_proud.c
+++ 0.Betty_would_be_proud.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
-
 /**
- * main - prints length of string
- * Return: length of string
+ * print_numbers -  prints numbers
+ * @numbers: integers to print
+ * Return: 0
+ */
+void print_numbers(int numbers[])
+{
+	int i;
+
+	for (int i = 0; i < max_size; i++)
+	{
+		printf("%d ", numbers[i]);
+	}
+	printf("\n");
+}
+/**
+ * main - print numbers
+ * Return: 0
  */
 int main(void)
 {
-	int i;
-	char s[] = "Programming is fun";
+	int numbers[max_size] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		printf("Length of the string: %s", i);
-	}
-
+	print_numbers(numbers);
 	return (0);
 }
-
